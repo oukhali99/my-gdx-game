ff4c140f82e882a2a13fecd0bc249fdea325490e
Oussama Khalifeh <okhalifeh@assetscience.com>
2023-04-22 13:20:23 -0400

    Basic rain drop game

--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -2,7 +2,6 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.mygdx.game.Game;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
@@ -10,6 +9,8 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("my-gdx-game");
-		new Lwjgl3Application(new Game(), config);
+		config.setWindowedMode(800, 480);
+		config.useVsync(true);
+		new Lwjgl3Application(new Drop(), config);
 	}
 }
--- html/src/com/mygdx/game/client/HtmlLauncher.java
+++ html/src/com/mygdx/game/client/HtmlLauncher.java
@@ -3,20 +3,17 @@ package com.mygdx.game.client;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
-import com.mygdx.game.Game;
+import com.mygdx.game.Drop;
 
 public class HtmlLauncher extends GwtApplication {
 
         @Override
         public GwtApplicationConfiguration getConfig () {
-                // Resizable application, uses available space in browser
-                return new GwtApplicationConfiguration(true);
-                // Fixed size application:
-                //return new GwtApplicationConfiguration(480, 320);
+                return new GwtApplicationConfiguration(800, 480);
         }
 
         @Override
         public ApplicationListener createApplicationListener () {
-                return new Game();
+                return new Drop();
         }
 }
