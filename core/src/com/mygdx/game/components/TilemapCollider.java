04f179f45b999ddca1d35a949460ff49cb131910
Oussama Khalifeh <okhalifeh@assetscience.com>
2023-04-23 17:25:12 -0400

    Added drawing borders

--- core/src/com/mygdx/game/components/TilemapCollider.java
+++ core/src/com/mygdx/game/components/TilemapCollider.java
@@ -1,5 +1,7 @@
 package com.mygdx.game.components;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.maps.MapLayer;
 import com.badlogic.gdx.maps.MapLayers;
 import com.badlogic.gdx.maps.MapObject;
@@ -7,13 +9,14 @@ import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
 import com.mygdx.game.Drop;
 import com.mygdx.game.gameobjects.GameObject;
 
 public class TilemapCollider extends Component {
     private TiledMap tiledMap;
     private MapLayers mapLayers;
-    private MapLayer collisionLayer;
+    protected MapLayer collisionLayer;
 
     public TilemapCollider(Drop game, TiledMap tiledMap) {
         super(game);
--- core/src/com/mygdx/game/level/Level.java
+++ core/src/com/mygdx/game/level/Level.java
@@ -2,7 +2,13 @@ package com.mygdx.game.level;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.mygdx.game.Drop;
 import com.mygdx.game.components.Tilemap;
@@ -36,13 +42,61 @@ public abstract class Level {
         music = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
         music.setLooping(true);
 
+
         // Create the tilemap
         GameObject tileMap = new GameObject(game) {
         };
+
+        // Actual tilemap component
         Tilemap tilemapComponent = new Tilemap(game, camera, "map/map.tmx");
         tileMap.addComponent(tilemapComponent);
-        tileMap.addComponent(new TilemapCollider(game, tilemapComponent.getMap()));
+
+        // Collider component
+        TilemapCollider tilemapCollider = new TilemapCollider(game, tilemapComponent.getMap()) {
+            @Override
+            public void destroy() {
+                super.destroy();
+                shapeRenderer.dispose();
+            }
+
+            private ShapeRenderer shapeRenderer;
+
+            @Override
+            public void initialize() {
+                super.initialize();
+                shapeRenderer = new ShapeRenderer();
+            }
+
+            @Override
+            public void update(float delta) {
+                super.update(delta);
+
+                shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+
+                // Set the color to red
+                shapeRenderer.setColor(Color.RED);
+
+                // Draw a rectangle at position (x=100, y=100) with width=100 and height=50
+                for (MapObject mapObject : collisionLayer.getObjects()) {
+                    if (mapObject instanceof RectangleMapObject) {
+                        RectangleMapObject rectangleMapObject = (RectangleMapObject) mapObject;
+                        Rectangle rectangle = rectangleMapObject.getRectangle();
+
+                        Vector3 rectPos = new Vector3(rectangle.x, rectangle.y, 0);
+                        camera.project(rectPos);
+
+                        //shapeRenderer.rect(rectPos.x, rectPos.y, rectangle.width, rectangle.height);
+                    }
+                }
+
+                // End the shape renderer
+                shapeRenderer.end();
+            }
+        };
+        tileMap.addComponent(tilemapCollider);
+
         gameObjects.add(tileMap);
+
 
         // Create the player
         player = new Player(game);
